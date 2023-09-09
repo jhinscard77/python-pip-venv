2f55baea94c026f220e5f29fd2ffcc4cdebb05ca
Jim WSL ROG <jhinscard77@hotmail.com>
2023-09-09 08:54:07 -0500

    actuliza venv en git ignore y agrega web-server

---
+++ web-server/store.py
@@ -0,0 +1,12 @@
+import requests
+
+
+def get_categories():
+    link = 'https://api.escuelajs.co/api/v1/categories'
+    r = requests.get(link)
+    print(r.status_code)
+    print(r.text)
+    print(type(r.text))
+    categories = r.json()
+    for category in categories:
+        print(category['name'])
